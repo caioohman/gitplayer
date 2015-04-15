799c0603b78dd063d7252f3f9500cd3417b89186
caioohman <c.balthazar70@gmail.com>
2015-04-15 00:19:09 +0000

    same as death.bash

--- exec.bash
+++ exec.bash
@@ -1,37 +1,43 @@
 #!/bin/bash
 
-function random()
+function exec()
 {
 
 HDMI_TERM="/dev/tty1"
 DEBUG_TERM="/dev/tty0"
 PEN_DIR="/mnt/pd"
-PEN_SEC="/mnt/pd2/"
 
 c="0"
+
+if test -e /tmp/videolist1
+then
+
+clear
+
  
-
-COMMAND="$(find $PEN_DIR $PEN_SEC -maxdepth 1 -type f -iregex ".*\.\(mp4\|mkv\|avi\|mpg\)$" | wc -l)"
-clear 
-
-#print number of files
-echo "numero de arquivos de vídeo listados - $COMMAND"
-
-#need to list subdirectories
-cd $PEN_DIR && ls -1 *.{mp4,mkv,avi,mpg} > /tmp/videolist  2> /dev/null
-cd $PEN_SEC && ls -1 *.{mp3,acc,flac,ogg}  >> /tmp/list  2> /dev/null
+#counter the number of files
+COMMAND="$(wc -l < /tmp/videolist1)"
 
 #Internal Field Separator
-IFS=$'\n' read -d '' -r -a lines < /tmp/videolist
+IFS=$'\n' read -d '' -r -a lines < /tmp/videolist1
+
+cd $PEN_DIR
 
 while [ $c -lt "$COMMAND" ]
 do 
  
-printf " %s\n" "$d -  ${lines[c]}" 
-mediainfo  "${lines[c]}" | sed -n ' /Complete name/,/Duration/p'
+printf " %s\n" "$d -  ${lines[c]}"  > $HDMI_TERM
+mediainfo  "${lines[c]}" | sed -n ' /Complete name/,/Duration/p'  > $HDMI_TERM
 
-nice -15 mplayer -really-quiet -vo fbdev2 -ao alsa:device=hw=1.0 -autosync 1 -framedrop "${lines[first]}" &
+nice -15 mplayer -really-quiet -vo fbdev2 -ao alsa:device=hw=1.0 -autosync 1 -framedrop "${lines[first]}" 
 
+c=$[$c+1]
+
+done
+/home/ubuntu/bin/death.bash
+else
+/home/ubuntu/bin/death.bash
+
+fi
 }
-
-random
+exec
