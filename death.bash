bd231139e5c2ba2ba0bf4c9bd9bd0df405d43b98
caioohman <c.balthazar70@gmail.com>
2015-04-15 07:31:43 +0000

    just play audio files

--- death.bash
+++ death.bash
@@ -7,12 +7,13 @@ DEBUG_TERM="/dev/tty0"
 PEN_SEC="/mnt/pd2"
 
 c="0"
+d="1"
 
 
 if test -e /tmp/videolist2
 then
 
-clear
+clear > $HDMI_TERM
 
 
 #count the number of files
@@ -26,11 +27,18 @@ cd $PEN_SEC
 while [ $c -lt "$COMMAND" ]
 do
 
-printf " %s\n" "$d -  ${lines[c]}"  > $HDMI_TERM
+echo '' > $HDMI_TERM
+echo '' > $HDMI_TERM
+
+
+printf " %s\n" "$d de $COMMAND -  ${lines[c]}"  > $HDMI_TERM
 mediainfo  "${lines[c]}" | sed -n ' /Complete name/,/Duration/p'  > $HDMI_TERM
 
+sleep 1
+clear > $HDMI_TERM
 nice -15 mplayer -really-quiet -vo fbdev2 -ao alsa:device=hw=1.0 -autosync 1 -framedrop "${lines[first]}"
 
+c=$[$c+1]
 c=$[$c+1]
 done
 /home/ubuntu/bin/startup.bash
