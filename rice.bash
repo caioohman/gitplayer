865553910431fcfa602cdb47f914bb496f84c816
caioohman <c.balthazar70@gmail.com>
2015-04-15 07:34:19 +0000

    just play audio files2

--- rice.bash
+++ rice.bash
@@ -12,12 +12,14 @@ PEN_DIR="/mnt/pd"
 if test -e /tmp/list1
 then
 
-clear
+clear > $HDMI_TERM
+
 
 c="0"
+d="1"
 
 #count the number of files
-COMMAND="$(wc -l < /tmp/videolist2)"
+COMMAND="$(wc -l < /tmp/list1)"
 
 #Internal Field Separator
 IFS=$'\n' read -d '' -r -a lines < /tmp/list1
@@ -27,12 +29,18 @@ cd $PEN_DIR
 while [ $c -lt "$COMMAND" ]
 do
 
-printf " %s\n" "$d -  ${lines[c]}"  > $HDMI_TERM
+echo '' > $HDMI_TERM
+echo '' > $HDMI_TERM
+
+printf " %s\n" "$d de $COMMAND -  ${lines[c]}"  > $HDMI_TERM
 mediainfo "${lines[c]}" | sed -n ' /Track name/,/Genre/p'  > $HDMI_TERM
+ 
+
 
 mplayer -really-quiet -ao alsa:device=hw=1.0 "${lines[c]}"
 
 c=$[$c+1]
+d=$[$d+1]
 done
 
 /home/ubuntu/bin/play.bash
