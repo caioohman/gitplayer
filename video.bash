d7af65754cddc909295ed75ee767e4a9237a5c3a
caioohman <c.balthazar70@gmail.com>
2015-04-15 00:22:11 +0000

    choose video option

--- video.bash
+++ video.bash
@@ -6,88 +6,107 @@ function videos()
 HDMI_TERM="/dev/tty1"
 DEBUG_TERM="/dev/tty0"
 PEN_DIR="/mnt/pd"
-PEN_SEC="/mnt/pd2/"
 
-c="0" 
+c="0"
 d="1"
 e="1"
- 
 
-COMMAND="$(find $PEN_DIR $PEN_SEC -maxdepth 1 -type f -iregex ".*\.\(mp4\|mkv\|avi\|mpg\)$" | wc -l)"
- 
+if test -e /tmp/videolist1
+then
+
 clear
 
-#print number of files
-echo "numero de arquivos de vídeo listados - $COMMAND"
+COMMAND="$(wc -l < /tmp/videolist1)"
+IFS=$'\n' read -d '' -r -a lines < /tmp/videolist1
 
-#need to list subdirectories
-cd $PEN_DIR && ls -1 *.{mp4,mkv,avi,mpg} >> /tmp/videolist  2> /dev/null
-cd $PEN_SEC && ls -1 *.{mp3,acc,flac,ogg}  >> /tmp/list  2> /dev/null
-
-
-IFS=$'\n' read -d '' -r -a lines < /tmp/videolist
-
-echo "" 
-echo "  -   Este menu é de escolha dos arquivos de vídeo desejados  - " 
-echo "  -   A cada numero que escolher,  aperte enter - " 
-echo "  -   Senão quiser nenhum dos arquivos listados, somente aperte enter ou espere aparecer a próxima tela" 
-echo ""
-echo ""
+echo '' > $HDMI_TERM
+echo '  -   Este menu é de escolha dos arquivos de audio desejados  - '  > $HDMI_TERM
+echo '  -   A cada numero que escolher,  aperte enter -  '  > $HDMI_TERM
+echo '  -   Senão quiser nenhum dos listados, somente aperte enter ou espere aparecer a proxima tela'  > $HDMI_TERM
+echo ''  > $HDMI_TERM
+echo ''  > $HDMI_TERM
 
 
-sleep 4
+sleep 2
+
+cd $PEN_DIR
 
 while [ $c -lt "$COMMAND" ]
-do 
- 
-printf " %s\n" "$d -  ${lines[c]}" 
-mediainfo  "${lines[c]}" | sed -n ' /Complete name/,/Duration/p'
+do
 
-echo ""
-echo ""
+# sed command is not working properly ... but it is working!!
+printf " %s\n" "$d -  ${lines[c]}"  > $HDMI_TERM
+mediainfo "${lines[c]}" | sed -n ' /Track name/,/Genre/p'  > $HDMI_TERM
+
+echo ''  > $HDMI_TERM
+echo ''  > $HDMI_TERM
 
 
-if test $e -eq 2
+f=$(($c + 1 ))
+
+if [ $e -eq 4 ] || [ $e -lt 4 -a $COMMAND -eq $f ] ||  [ $COMMAND -lt 4 -a $e -eq $COMMAND ]
 then
-#
-echo "Digite os números desejados"
+
+echo 'Digite os números desejados'  > $HDMI_TERM
 
 # -t is for timeout (seconds)
 # -a parameter means array
 read  -t 30 choice_one
 read  -t 30 choice_two
+read  -t 30 choice_three
+read  -t 30 choice_four
 
 first=$(($choice_one - 1))
 second=$(($choice_two - 1))
+third=$(( $choice_three - 1))
+fourth=$(($choice_four - 1))
 
 sleep 2
 
 if test $first -gt -1
 then
-echo 'Executando primeira escolha'
-sleep 2
-nice -15 mplayer -really-quiet -vo fbdev2 -ao alsa:device=hw=1.0 -autosync 1 -framedrop "${lines[first]}" &
+echo 'Executando primeira escolha'  > $HDMI_TERM
+sleep 1
+nice -15 mplayer -really-quiet -vo fbdev2 -ao alsa:device=hw=1.0 -autosync 1 -framedrop "${lines[first]}"
 fi
 
 if test $second -gt -1
 then
-echo 'Executando segunda escolha'
-sleep 2
-nice -15 mplayer -really-quiet -vo fbdev2 -ao alsa:device=hw=1.0 -autosync 1 -framedrop "${lines[second]}" &
+echo 'Executando segunda escolha'  > $HDMI_TERM
+sleep 1
+nice -15 mplayer -really-quiet -vo fbdev2 -ao alsa:device=hw=1.0 -autosync 1 -framedrop "${lines[second]}"
+fi
+
+if test $third -gt -1
+then
+echo 'Executando terceira escolha'  > $HDMI_TERM
+sleep 1
+nice -15 mplayer -really-quiet -vo fbdev2 -ao alsa:device=hw=1.0 -autosync 1 -framedrop "${lines[third]}"
+fi
+
+if test $fourth -gt -1
+then
+echo 'Executando quarta escolha'  > $HDMI_TERM
+sleep 1
+nice -15 mplayer -really-quiet -vo fbdev2 -ao alsa:device=hw=1.0 -autosync 1 -framedrop "${lines[fourth]}"
 fi
 
 clear
-e="0"
-fi
+e="0" # because it will increment before start the loop again
 
-sleep 5
+fi
 
 c=$[$c+1]
 d=$[$d+1]
 e=$[$e+1]
-
 done
+/home/ubuntu/bin/life.bash
+#There is no audio file
+else
 
+/home/ubuntu/bin/life.bash
+
+fi
 }
 videos
 
