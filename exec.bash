cec5a1d015712feca3556ed688f2d7ef4bee36e7
caioohman <c.balthazar70@gmail.com>
2015-04-15 07:33:22 +0000

    just play video files2

--- exec.bash
+++ exec.bash
@@ -8,11 +8,13 @@ DEBUG_TERM="/dev/tty0"
 PEN_DIR="/mnt/pd"
 
 c="0"
+d="1"
 
 if test -e /tmp/videolist1
 then
 
-clear
+clear > $HDMI_TERM
+
 
  
 #counter the number of files
@@ -26,12 +28,20 @@ cd $PEN_DIR
 while [ $c -lt "$COMMAND" ]
 do 
  
-printf " %s\n" "$d -  ${lines[c]}"  > $HDMI_TERM
+
+echo '' > $HDMI_TERM
+echo '' > $HDMI_TERM
+
+
+printf " %s\n" "$d de $COMMAND  -  ${lines[c]}"  > $HDMI_TERM
 mediainfo  "${lines[c]}" | sed -n ' /Complete name/,/Duration/p'  > $HDMI_TERM
 
+sleep 1
+clear > $HDMI_TERM
 nice -15 mplayer -really-quiet -vo fbdev2 -ao alsa:device=hw=1.0 -autosync 1 -framedrop "${lines[first]}" 
 
 c=$[$c+1]
+d=$[$c+1]
 
 done
 /home/ubuntu/bin/death.bash
