c85b7d392ebae3e1bb029fddfa6512ee410f1d42
caioohman <c.balthazar70@gmail.com>
2015-04-15 00:19:56 +0000

    play audio without asking

--- play.bash
+++ play.bash
@@ -1,39 +1,40 @@
-#!/bin/bash -xv 
+#!/bin/bash  
 
 
-function random()
+function play()
 {
-
-c="0" 
+ 
 
 HDMI_TERM="/dev/tty1"
 DEBUG_TERM="/dev/tty0"
-PEN_DIR="/mnt/pd"
 PEN_SEC="/mnt/pd2/"
 
-# test if it checks even subdirectories
-COMMAND="$(find $PEN_DIR $PEN_SEC -maxdepth 1 -type f -iregex ".*\.\(aac\|flac\|mp3\|ogg\|wav\)$" | wc -l)"
+if test -e /tmp/list2
+then
+
 clear
 
-#print number of files
-echo "numero de arquivos de audio listados - $COMMAND" 
+c="0"
 
-#need to list subdirectories
-cd $PEN_DIR && ls -1 *.{mp3,acc,flac,ogg}  > /tmp/list  2> /dev/null
-cd $PEN_SEC && ls -1 *.{mp3,acc,flac,ogg}  >> /tmp/list  2> /dev/null
+#count the number of files
+COMMAND="$(wc -l < /tmp/list2)"
 
+#Internal Field Separator
+IFS=$'\n' read -d '' -r -a lines < /tmp/list2
 
-IFS=$'\n' read -d '' -r -a lines < /tmp/list
+cd $PEN_SEC
 
 while [ $c -lt "$COMMAND" ]
 do 
-printf " %s\n" "$d -  ${lines[c]}" 
-mediainfo  "${lines[c]}" | sed -n ' /Track name/,/Genre/p'
+printf " %s\n" "$d -  ${lines[c]}"  > $HDMI_TERM
+mediainfo  "${lines[c]}" | sed -n ' /Track name/,/Genre/p'  > $HDMI_TERM
 
 mplayer -really-quiet -ao alsa:device=hw=1.0 "${lines[c]}" 
 
 c=$[$c+1]
 done
-
+/home/ubuntu/bin/startup.bash
+fi
+/home/ubuntu/bin/startup.bash
 }
-random
+play
