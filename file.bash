911be7200b37d3e02ddb0c555486e609db8afa26
caioohman <c.balthazar70@gmail.com>
2015-05-18 01:34:17 +0000

    repeat function added

--- file.bash
+++ file.bash
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash -xv
 
 function files()
 {
@@ -10,6 +10,7 @@ PEN_DIR="/mnt/pd/"
 c="0" 
 d="1"
 e="1"
+false="1"
 
 if test -e /tmp/list1
 then
@@ -23,7 +24,7 @@ IFS=$'\n' read -d '' -r -a lines < /tmp/list1
 echo '' > $HDMI_TERM
 echo '  -   Este menu é de escolha dos arquivos de audio desejados  - ' > $HDMI_TERM
 echo '  -   A cada numero que escolher,  aperte enter -  ' > $HDMI_TERM
-echo '  -   Senão quiser nenhum dos listados, somente aperte enter ou espere aparecer a próxima tela' > $HDMI_TERM
+echo '  -   Senao quiser nenhum dos listados, somente aperte enter ou espere aparecer a próxima tela' > $HDMI_TERM
 echo '' > $HDMI_TERM
 echo '' > $HDMI_TERM
 
@@ -38,10 +39,8 @@ do
 printf " %s\n" "$d -  ${lines[c]}" > $HDMI_TERM
 mediainfo "${lines[c]}" | sed -n ' /Track name/,/Genre/p' > $HDMI_TERM
 
-
 echo '' > $HDMI_TERM
 echo '' > $HDMI_TERM
-
 
 f=$(($c + 1 ))
 
@@ -49,7 +48,6 @@ if [ $e -eq 4 ] || [ $e -lt 4 -a $COMMAND -eq $f ] ||  [ $COMMAND -lt 4 -a $e -e
 then
 #
 echo 'Digite os números desejados' > $HDMI_TERM
-
 
 # -t is for timeout (seconds)
 # -a parameter means array
@@ -76,35 +74,110 @@ if test $first -gt -1
 then
 echo 'Executando primeira escolha' > $HDMI_TERM
 sleep 1
+#repeat loop
+while [ $false -eq 1 ]
+do
+
 mplayer -really-quiet -ao alsa:device=hw=1.0 "${lines[first]}"
+
+#1 - no
+#2 - yes
+echo 'Repetir : (1)Nao (2)Sim' > $HDMI_TERM
+echo 'Repetir : (1)Não (2)Sim' 
+
+read -t 10 answer
+result=$(($answer - 1))
+
+if [ $answer -ne 1 ]
+then
+false=0
+fi
+done
+false=1
 fi
 
 if test $second -gt -1
 then
 echo 'Executando segunda escolha' > $HDMI_TERM
 sleep 1
+#repeat loop
+while [ $false -eq 1 ]
+do
+
 mplayer -really-quiet -ao alsa:device=hw=1.0 "${lines[second]}"
+
+#1 - no
+#2 - yes
+echo 'Repetir : (1)Nao (2)Sim'> $HDMI_TERM
+echo 'Repetir : (1)Não (2)Sim' 
+
+read -t 10 answer
+result=$(($answer - 1))
+
+if [ $answer -ne 1 ]
+then
+false=0
+fi
+done
+false=1
 fi
 
 if test $third -gt -1
 then
 echo 'Executando terceira escolha' > $HDMI_TERM
 sleep 1
+#repeat loop
+while [ $false -eq 1 ]
+do
+
 mplayer -really-quiet -ao alsa:device=hw=1.0 "${lines[third]}"
+
+#1 - no
+#2 - yes
+echo 'Repetir : (1)Nao (2)Sim' > $HDMI_TERM
+echo 'Repetir : (1)Não (2)Sim' 
+
+read -t 10 answer
+result=$(($answer - 1))
+
+if [ $answer -ne 1 ]
+then
+false=0
+fi
+done
+false=1
 fi
 
 if test $fourth -gt -1
 then
 echo 'Executando quarta escolha' > $HDMI_TERM
 sleep 1
+#repeat loop
+while [ $false -eq 1 ]
+do
+
 mplayer -really-quiet -ao alsa:device=hw=1.0 "${lines[fourth]}"
+
+#1 - no
+#2 - yes
+echo 'Repetir : (1)Nao (2)Sim' > $HDMI_TERM
+echo 'Repetir : (1)Não (2)Sim' 
+
+read -t 10 answer
+result=$(($answer - 1))
+
+if [ $answer -ne 1 ]
+then
+false=0
+fi
+done
+false=1
 fi
 
 clear > $HDMI_TERM
 e="0" # because it will increment before start the loop again
 
 fi
-
 
 c=$[$c+1]
 d=$[$d+1]
