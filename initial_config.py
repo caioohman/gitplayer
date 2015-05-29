3f085a2c9b38a5b54a3a6a936777f7bf83b7d635
caioohman <c.balthazar70@gmail.com>
2015-05-29 13:09:33 +0000

    still in process

--- initial_config.py
+++ initial_config.py
@@ -3,6 +3,22 @@
 
 import subprocess
 
+arquivo = "/tmp/test.txt"
+
+
+def file_open(file):
+	#fo because of file object
+	fo = open( file , "w")
+	if fo > 0:
+		print "OK"
+		fo.write( "\nO tamanho escolhido foi :%s...\n" %font)
+	else:
+		print "ERRO"
+
+	fo.close()
+
+	return 1
+
 print """Ajuste o tamanho da fonte de acordo com as opções
 
 tamanho :14
@@ -21,9 +37,14 @@ tamanho :32x16"""
 
 font = raw_input("Digite o tamanho:")
 
-command = "/usr/share/consolefonts/Lat7-TerminusBold%s.psf" %font
-#print command
+if font in ( '32x16' , '28x14' , '24x12' , '22x11' , '20x10' , '16' , '14' ):
 
-#because I don´t need to check the output
-subprocess.call(["setfont" , command ])
+	command = "/usr/share/consolefonts/Lat7-TerminusBold%s.psf" %font
+        #because I don´t need to check the output
+	subprocess.call(["setfont" , command ])
+        abrir=file_open(arquivo)
+        
+else:
+	print "Não foi posto nenhum dos tamanhos listados"
+
 
