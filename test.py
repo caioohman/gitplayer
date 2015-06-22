d68a5be42f4e35cb556a82356fe483e0f7a877d5
caioohman <c.balthazar70@gmail.com>
2015-06-22 23:26:10 +0000

    ops

--- test.py
+++ test.py
@@ -7,8 +7,10 @@ teste = subprocess.Popen( ["find /root/ -iname player" ] , shell=True ,  stdout=
 
 result = output.split()
 
+#print result
+
 if not result: 
-	symlink = subprocess.Popen(["ln -s /home/ubuntu/gitplayer/startup.bash player"] , shell=True , stdout=subprocess.PIPE)	 
+	symlink = subprocess.Popen(["ln -s /home/ubuntu/gitplayer/startup.bash /root/player"] , shell=True , stdout=subprocess.PIPE)	 
 	( out , err ) = symlink.communicate()
 	#print symlink
 else: print "\nOK\n"
